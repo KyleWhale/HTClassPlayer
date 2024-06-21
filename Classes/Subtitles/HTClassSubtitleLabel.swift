e565b243c4031964651501385153984547041adf
李雪健 <kylej@bluewhaletech.pro>
2024-06-21 11:05:06 +0800

    字幕Label

--- Classes/Subtitles/HTClassSubtitleLabel.swift
+++ Classes/Subtitles/HTClassSubtitleLabel.swift
@@ -12,6 +12,27 @@ public class HTClassSubtitleLabel: UILabel {
     
     public var var_edgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        ht_init()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        ht_init()
+    }
+    
+    func ht_init() {
+        numberOfLines = 0
+        textColor = .white
+        textAlignment = .center
+        layer.cornerRadius = 6
+        layer.masksToBounds = true
+        adjustsFontSizeToFitWidth = true
+        font = UIFont.systemFont(ofSize: 14)
+        backgroundColor = UIColor.black.withAlphaComponent(0.4)
+    }
+    
     public override func drawText(in rect: CGRect) {
         let var_insetRect = rect.inset(by: var_edgeInsets)
         super.drawText(in: var_insetRect)
